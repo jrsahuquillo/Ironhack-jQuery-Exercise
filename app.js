be5538edb238b5088f04c70d1d4c6ae68d3d9eb9
jrsahuquillo <jrsahuquillo@gmail.com>
2016-11-21 21:17:52 +0100

    Iteration 3 ok

--- app.js
+++ app.js
@@ -8,4 +8,15 @@ $(document).ready(function(){
   $('button').on('click', function(){
       $('.phrases').html(phrases[Math.floor(Math.random()*(phrases.length))]);
   });
+
+
+  $('.input-text').keypress(function(event){
+    // event.preventDefault();
+    var newPhrase = $('.input-text').val();
+    if (event.which == 13) {
+      phrases.push(newPhrase);
+    }
+    $('.phrases').html(newPhrase);
+  });
+
 });
